33cae70083a1b16991c1489f09e22e67e169cc15
Sundar <Sundar@Sairam>
2021-05-17 19:01:25 +0530

    Maven_Git

---
+++ App/src/main/java/com/data/App.java
@@ -0,0 +1,11 @@
+package com.data;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello welcome");
+		System.out.println("Hello Second time");
+	}
+
+}
